2d529b647bdcbbbdf0b1f0f3704b7583976228c1
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-03-18 23:54:40 +0000

    Add javadoc comments to the class and the constructor of Http2Settings

--- src/main/java/com/app/Http2Settings.java
+++ src/main/java/com/app/Http2Settings.java
@@ -1,5 +1,18 @@
 package com.app;
 
+/**
+ * The Http2Settings class encapsulates the HTTP/2 protocol settings parameters
+ *
+ * <p>
+ * This class provides storage and access for the standard HTTP/2 settings as
+ * defined in
+ * <a href="https://tools.ietf.org/html/rfc7540#section-6.5.2>RFC 7540 Section
+ * 6.5.2</a>.
+ * It initialises all settings to their default values according to the HTTP/2
+ * specification,
+ * and provides methods to get and set each setting.
+ * </p>
+ */
 public class Http2Settings {
 
     public static final int SETTINGS_HEADER_TABLE_SIZE = 0x1;
@@ -16,6 +29,19 @@ public class Http2Settings {
     private int maxFrameSize;
     private int maxHeaderListSize;
 
+    /**
+     * Constructs a new Http2Settings instance with defult values as specified by
+     * the HTTP/2 protocol
+     * 
+     * <ul>
+     * <li>Initial Window Size: 65535 bytes</li>
+     * <li>Header Table Size: 4096 bytes</li>
+     * <li>Enable Push: true</li>
+     * <li>Max Concurrent Stream: Integer.MAX_VALUE</li>
+     * <li>Max Frame Size: 16384 bytes</li>
+     * <li>Max Header List Size: 8192 bytes</li>
+     * </ul>
+     */
     public Http2Settings() {
         initialWindowSize = 65535;
         headerTableSize = 4096;
