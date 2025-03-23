cad52a6b33aa4de01d498ae34df289bc7c0f71ee
Leonard Marshall Afzal <86576612+LMarshallAfzal@users.noreply.github.com>
2025-03-23 04:04:57 +0000

    Update Main the handler methods for HTTP/1 and HTTP/2
    - Create handleHttp1Connection method
    - Create handleHttp2Connection method
    - Refactor main method to call the appropriate handler method based on protocol version

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -37,11 +37,14 @@ public class Main {
         try {
             Acceptor acceptor = new Acceptor(enableSSL);
             ConnectionManager connectionManager = new ConnectionManager();
+            Http2ConnectionManager http2ConnectionManager = new Http2ConnectionManager();
+
             System.out.println("Server started " + (enableSSL ? "with SSL" : "without SSL"));
 
             while (true) {
                 Socket clientSocket = acceptor.acceptConnection();
 
+                // Check if client is connected
                 if (!connectionManager.isClientConnected(clientSocket)) {
                     int threadId = threadCounter.incrementAndGet();
                     System.out.println(
@@ -50,50 +53,22 @@ public class Main {
                     new Thread(() -> {
                         try {
                             System.out.println("Thread #" + threadId + " started");
-
                             clientSocket.setSoTimeout(30000);
-                            Processor processor = new Processor();
-                            Responder responder = new Responder();
 
-                            PushbackInputStream pbInputStream = new PushbackInputStream(
-                                    clientSocket.getInputStream(), 1);
-                            OutputStream outputStream = clientSocket.getOutputStream();
-
-                            int requestCount = 0;
-
-                            while (!clientSocket.isClosed()) {
-                                try {
-                                    int peekedByte = pbInputStream.read();
-                                    if (peekedByte != -1) {
-                                        requestCount++;
-
-                                        pbInputStream.unread(peekedByte);
-
-                                        System.out.println(
-                                                "Thread #" + threadId + " processing request #" + requestCount);
-                                        HttpRequest request = processor.parseRequest(pbInputStream);
-                                        HttpResponse response = processor.processRequest(request);
-                                        responder.sendResponse(response, outputStream);
-
-                                        boolean keepAlive = request.getRequestHeaders().containsKey("Connection")
-                                                && request.getRequestHeaders().get("Connection").equals("keep-alive");
-
-                                        if (!keepAlive) {
-                                            break;
-                                        }
-                                    } else {
-                                        System.out.println("Client closed the connection");
-                                        break;
-                                    }
-                                } catch (SocketTimeoutException e) {
-                                    System.out.println("Connection to " + clientSocket.getInetAddress().getHostName()
-                                            + " timed out");
-                                    break;
-                                } catch (IOException e) {
-                                    System.out.println("IO error processing request: " + e.getMessage());
-                                    break;
-                                }
+                            // Determine if this is an HTTP/2 connection
+                            boolean isHttp2 = false;
+                            if (clientSocket.getProperty("protocol") != null
+                                    && clientSocket.getProperty("protocol").equals("h2")) {
+                                isHttp2 = true;
                             }
+
+                            // Chooose handler based on HTTP protocol version
+                            if (isHttp2) {
+                                handleHttp2Connection(clientSocket, http2ConnectionManager, threadId);
+                            } else {
+                                handleHttp1Connection(clientSocket, connectionManager, threadId);
+                            }
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         } finally {
@@ -120,4 +95,85 @@ public class Main {
         }
     }
 
+    private static void handleHttp1Connection(Socket clientSocket, ConnectionManager connectionManager, int threadId)
+            throws IOException {
+        Processor processor = new Processor();
+        Responder responder = new Responder();
+
+        PushbackInputStream pbInputStream = new PushbackInputStream(clientSocket.getInputStream(), 1);
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        int requestCount = 0;
+
+        while (!clientSocket.isClosed()) {
+            try {
+                int peekedByte = pbInputStream.read();
+                if (peekedByte != -1) {
+                    requestCount++;
+
+                    pbInputStream.unread(peekedByte);
+
+                    System.err.println("Thread #" + threadId + " processing HTTP/1.1 request #" + requestCount);
+                    HttpRequest request = processor.parseRequest(pbInputStream);
+                    HttpResponse response = processor.processRequest(request);
+                    responder.sendResponse(response, outputStream);
+
+                    boolean keepAlive = request.getRequestHeaders().containsKey("Connection")
+                            && request.getRequestHeaders().get("Connection").equals("keep-alive");
+
+                    if (!keepAlive) {
+                        break;
+                    }
+
+                } else {
+                    System.out.println("Clinet closed the connection");
+                    break;
+                }
+            } catch (SocketTimeoutException e) {
+                System.err.println("Connection to " + clientSocket.getInetAddress().getHostName() + " timed out");
+                break;
+            } catch (IOException e) {
+                System.err.println("IO error processing request: " + e.getMessage());
+                break;
+            }
+        }
+        connectionManager.removeClient(clientSocket);
+    }
+
+    private static void handleHttp2Connection(Socket clientSocket, Http2ConnectionManager connectionManager,
+            int threadId) throws IOException {
+        connectionManager.addConnectedClient(clientSocket);
+
+        InputStream inputStream = clientSocket.getInputStream();
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        Http2Processor processor = new Http2Processor(connectionManager);
+        Http2Responder responder = new Http2Responder(connectionManager);
+
+        processor.initialise(outputStream);
+
+        // Process HTTP/2 frames
+        try {
+            while (!clientSocket.isClosed()) {
+                try {
+                    HttpResponse response = processor.processNextFrame(inputStream);
+
+                    // If there is a response to send, send it
+                    if (response != null) {
+                        Http2Stream stream = connectionManager.getStream(1);
+                        responder.sendResponse(response, stream, outputStream);
+                    }
+                } catch (SocketTimeoutException e) {
+                    System.out.println(
+                            "HTTP/2 connection to " + clientSocket.getInetAddress().getHostName() + " timed out");
+                    break;
+                } catch (IOException e) {
+                    System.err.println("IO error processing HTTP/2 frame: " + e.getMessage());
+                    break;
+                }
+            }
+        } finally {
+            connectionManager.removeClient(clientSocket);
+        }
+    }
 }
